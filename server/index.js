6c0def0393b73532e00db963c4d1a0b9a22f8729
Madhanraj R <104816054+MADHANRAJ-R@users.noreply.github.com>
2024-01-04 15:44:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors(
   {
     origin: ["https://structuralguru-rcc-sample-client.vercel.app/"],
-    methods: ["POST"],
+    methods: ["POST","GET","PUT","DELETE"],
     credentials: true
   }
 ));
